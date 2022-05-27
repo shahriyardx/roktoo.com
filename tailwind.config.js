4a5f213db46a684cf141acdfa12f07bacfe01bff
Md Shahriyar Alam <mdshahriyaralam552@gmail.com>
2022-05-27 13:55:04 +0600

    Setup tailwindcss and code cleanup

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    "./pages/**/*.js",
+    "./components/**/*.{js,jsx}",
+    "./sections/**/*.{js,jsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
