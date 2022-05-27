2aa1cf39caafe9c4369f0061b1821b63a0786b2f
Md Shahriyar Alam <mdshahriyaralam552@gmail.com>
2022-05-27 14:55:01 +0600

    Updated footer component

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,7 +1,21 @@
 import React from "react";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="flex justify-center py-5">
+      <span>
+        Made with 💝 by{" "}
+        <a
+          href="https://facebook.com/shahriyardx"
+          target="_blank"
+          rel="noreferrer"
+          className="text-red-700 font-bold"
+        >
+          Shahriar
+        </a>
+      </span>
+    </div>
+  );
 };
 
 export default Footer;
