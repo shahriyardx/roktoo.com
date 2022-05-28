a9bf8424b68af7e2eff11eaa19f843f8499a24d5
Md Shahriyar Alam <mdshahriyaralam552@gmail.com>
2022-05-29 04:03:28 +0600

    Dark mode loading component

--- components/Loading.jsx
+++ components/Loading.jsx
@@ -4,7 +4,7 @@ import { BiLoaderAlt } from "react-icons/bi";
 const Loading = () => {
   return (
     <div className="flex justify-center items-center py-20">
-      <div className="flex items-center gap-2">
+      <div className="flex items-center gap-2 dark:text-zinc-300">
         <BiLoaderAlt className="text-2xl animate-spin" />
         <span>Loading...</span>
       </div>
