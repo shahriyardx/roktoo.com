5a9f05772a34eaaae4033e6f72ea780632ca0ae1
Md Shahriyar Alam <mdshahriyaralam552@gmail.com>
2022-05-31 02:10:59 +0600

    Improved swipe

--- components/Layouts/ProfileLayout.jsx
+++ components/Layouts/ProfileLayout.jsx
@@ -9,11 +9,8 @@ const ProfileLayout = ({ children }) => {
   const [touchStart, setTouchStart] = useState(null);
   const [touchEnd, setTouchEnd] = useState(null);
 
-  // the required distance between touchStart and touchEnd to be detected as a swipe
-  const minSwipeDistance = 50;
-
   const onTouchStart = (e) => {
-    setTouchEnd(null); // otherwise the swipe is fired even with usual touch events
+    setTouchEnd(null);
     setTouchStart(e.targetTouches[0].clientX);
   };
 
@@ -22,16 +19,9 @@ const ProfileLayout = ({ children }) => {
   const onTouchEnd = () => {
     if (!touchStart || !touchEnd) return;
     const distance = touchStart - touchEnd;
-    const isLeftSwipe = distance > minSwipeDistance;
-    const isRightSwipe = distance < -minSwipeDistance;
-    if (isLeftSwipe) {
-      setOpen(false);
-    }
-
-    if (isRightSwipe) {
-      setOpen(true);
-    }
-    // add your conditional logic here
+    const isLeftSwipe = distance > 50;
+    const isRightSwipe = distance < -50;
+    isLeftSwipe ? setOpen(false) : isRightSwipe ? setOpen(true) : null;
   };
 
   return (
