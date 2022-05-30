98138742cf4624144b0c2421b9736f334823377e
Md Shahriyar Alam <mdshahriyaralam552@gmail.com>
2022-05-30 14:49:28 +0600

    Added lang and apple touch icon

--- pages/_document.js
+++ pages/_document.js
@@ -2,9 +2,10 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html>
+    <Html lang="en">
       <Head>
         <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" href="/icon-192x192.png"></link>
         <meta name="theme-color" href="#ff0000" />
       </Head>
       <body>
